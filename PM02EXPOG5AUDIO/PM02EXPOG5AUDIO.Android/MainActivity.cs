e227d3cc2127da6d0ed9c9a91bee87621dee8282
Marvin Josue Martinez Zapata <101903394+doguidog@users.noreply.github.com>
2023-03-18 13:09:55 -0600

    Add project files.

---
+++ PM02EXPOG5AUDIO/PM02EXPOG5AUDIO.Android/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
+using Android;
+
+namespace PM02EXPOG5AUDIO.Droid
+{
+    [Activity(Label = "PM02EXPOG5AUDIO", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            //Da los permisos de audio del telefono
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.RecordAudio) != Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.RecordAudio }, 1);
+            }
+
+
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ PM02EXPOG5AUDIO/PM02EXPOG5AUDIO/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Plugin.AudioRecorder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM02EXPOG5AUDIO
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        AudioRecorderService gravador;
+        AudioPlayer reprodutor;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            gravador = new AudioRecorderService
+            {
+                StopRecordingAfterTimeout = true,
+                TotalAudioTimeout = TimeSpan.FromSeconds(15),
+                AudioSilenceTimeout = TimeSpan.FromSeconds(15)
+            };
+
+            reprodutor = new AudioPlayer();
+            reprodutor.FinishedPlaying += Finaliza_Reproducao;
+
+        }
+
+        private async void GravarButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!gravador.IsRecording)
+                {
+                    gravador.StopRecordingOnSilence = TimeoutSwitch.IsToggled;
+
+                    GravarButton.IsEnabled = false;
+                    ReproduzirButton.IsEnabled = false;
+
+                    //Começa gravação
+                    var audioRecordTask = await gravador.StartRecording();
+
+                    GravarButton.Text = "Parar de grabar";
+                    GravarButton.IsEnabled = true;
+
+                    await audioRecordTask;
+
+                    GravarButton.Text = "Grabar Audio";
+                    ReproduzirButton.IsEnabled = true;
+                }
+                else
+                {
+                    GravarButton.IsEnabled = false;
+
+                    //parar a gravação...
+                    await gravador.StopRecording();
+
+                    GravarButton.IsEnabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                //blow up the app!
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void ReproduzirButton_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var filePath = gravador.GetAudioFilePath();
+
+                if (filePath != null)
+                {
+                    ReproduzirButton.IsEnabled = false;
+                    GravarButton.IsEnabled = false;
+
+                    reprodutor.Play(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                //blow up the app!
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+
+        void Finaliza_Reproducao(object sender, EventArgs e)
+        {
+            ReproduzirButton.IsEnabled = true;
+            GravarButton.IsEnabled = true;
+        }
+
+
+    }
+}
